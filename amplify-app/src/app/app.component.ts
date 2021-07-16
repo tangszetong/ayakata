ce1a840bcc1592d1620b28f9b47566b129677817
Szetong123 <63947413+Szetong123@users.noreply.github.com>
2021-07-16 18:26:04 +0800

    initial commit

---
+++ amplify-app/src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { APIService } from './API.service';
+import { Restaurant } from '../types/restaurant';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit {
+  title = 'amplify-angular-app';
+  public createForm!: FormGroup;
+
+  /* declare restaurants variable */
+  restaurants: Array<Restaurant> = [];
+
+  constructor(private api: APIService, private fb: FormBuilder) { }
+
+  async ngOnInit() {
+    this.createForm = this.fb.group({
+      'name': ['', Validators.required],
+      'description': ['', Validators.required],
+      'city': ['', Validators.required]
+    });
+    this.api.ListRestaurants().then( (event: any) => {
+      this.restaurants = event.items;
+    });
+  
+    /* subscribe to new restaurants being created */
+    this.api.OnCreateRestaurantListener.subscribe( (event: any) => {
+      const newRestaurant = event.value.data.onCreateRestaurant;
+      this.restaurants = [newRestaurant, ...this.restaurants];
+    });
+  }
+
+  public onCreate(restaurant: Restaurant) {
+    this.api.CreateRestaurant(restaurant).then((_event: any) => {
+      console.log('item created!');
+      this.createForm.reset();
+    })
+    .catch((e: any) => {
+      console.log('error creating restaurant...', e);
+    });
+  }
+}
---
+++ amplify-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+
+/* new form imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AmplifyUIAngularModule,
+    /* configuring form modules */
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
